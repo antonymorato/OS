b874666efcacefee6b0847fb1cb48b8066b91daf
antony <antonymorato2@gmail.com>
2019-11-08 15:02:40 +0200

    last version of writers

--- writers.cpp
+++ writers.cpp
@@ -1,32 +1,41 @@
-//reader-writer
-
+//
+//  main.cpp
+//  reader-writer
+//
+//  Created by Anton Korol 
+//  Copyright © 2019 Anton Korol. All rights reserved.
+//
 #include <windows.h>
 #include <iostream>
 #include <stdio.h>
 #include <conio.h>
-
+#include <mutex>
 #define MAX_WRITERS 2
-#define MAX_READERS 4
-
+#define MAX_READERS 8
+using namespace std;
 void writer(char *name);
 void reader(char *name);
 DWORD uThrWriterID[MAX_WRITERS], uThrReaderID[MAX_READERS];
-HANDLE semArr[2];
-HANDLE semWrite,semRead,semForRead;
+
+
+HANDLE a,b,c;
+
 HANDLE hThrWritersArray[MAX_WRITERS], hThrReadersArray[MAX_READERS];
-int readcount=0,writecount=0,container;
+int flag=0,readcount=0;
+mutex mtx;
 int main()
-{	container=0;
-	semWrite=CreateSemaphore(NULL,1,1,NULL);
-	semRead=CreateSemaphore(NULL,1,MAX_READERS,NULL);
-	semForRead=CreateSemaphore(NULL,1,1,NULL);
+{	
+ 
+	a=CreateSemaphore(NULL,1,1,NULL);
+	b=CreateSemaphore(NULL,1,1,NULL);
+	c=CreateSemaphore(NULL,1,1,NULL);
 	
-	semArr[0]=semRead;
-	semArr[1]=semForRead;
+	
 	
 	for ( int i = 0; i < MAX_WRITERS; i++ )
     {
         char* prod = new char[11];
+        
         sprintf(prod, "Writer %d", i+1);
         hThrWritersArray[i] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)writer, prod, 0, &uThrWriterID[i]);
     }
@@ -35,11 +44,12 @@ int main()
  	char* cons=new char[11];
  	sprintf(cons,"Reader %d",i+1);
     hThrReadersArray[i] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)reader, cons, 0, &uThrReaderID[i]);
+    Sleep(rand()%500);
 }
 	getch();
-	CloseHandle(semWrite);
-    CloseHandle(semRead);
-    CloseHandle(semForRead);
+	CloseHandle(a);
+    CloseHandle(b);
+    CloseHandle(c);
     for ( int i = 0; i < MAX_WRITERS; i++ )
         CloseHandle(hThrWritersArray[i]);
  
@@ -50,72 +60,60 @@ int main()
 
 void reader(char *name)
 {	while(true)
-	{ 
-		WaitForMultipleObjects(2,semArr,TRUE,INFINITE);
-		Sleep(rand()%1000);
-		printf("%s\tValue:%d\n\n",name,container);
-		Sleep(rand()%2500);
-		ReleaseSemaphore(semRead,1,NULL);
-		ReleaseSemaphore(semForRead,1,NULL);
+	{ 	if(flag==1) continue;
+		WaitForSingleObject(b,INFINITE);
+		if(readcount==0){
+		
+		WaitForSingleObject(a,INFINITE);
+
+	}
+		readcount++;
+
+		ReleaseSemaphore(b,1,NULL);
+		
+
+		WaitForSingleObject(c,INFINITE);
+
+		cout<<"There are "<<readcount<<" readers in work."<<endl; 
+
+		ReleaseSemaphore(c,1,NULL);
+		Sleep(rand()%500);
+		WaitForSingleObject(b,INFINITE);
+			readcount--;
+			if(readcount==0)
+			{
+				ReleaseSemaphore(a,1,NULL);
+
+			}
+		ReleaseSemaphore(b,1,NULL);
+		Sleep(rand()%500);
 		}
 	
 	
-//	int num1=*(int*)prm;
-//	int i=0,r;
-//	for(i;i<iter;i++)
-//	{
-		
-//		if (sem_wait(&orderM)==0) printf("%d Читатель %d в очереди__________Ч%d\n",i,num1,num1);	// Remember our order of arrival
-//		sem_wait(&readresM);				 // We will manipulate the readers counter
-//		if (readers == 0)				// If there are currently no readers (we came first)...
-//			sem_wait(&accessM);				// ...requests exclusive access to the resource for readers
-//		readers++;							 // Note that there is now one more reader
-//		sem_post(&orderM);					 // Release order of arrival semaphore (we have been served)
-//		sem_post(&readresM);				 // We are done accessing the number of readers for now
-//
-//		printf("%d Работает читатель %d________________Ч%d\n",i,num1,num1);				// Here the reader can read the resource at will
-//		r=1+rand()%4;
-//		sleep(r);
-//		sem_wait(&readresM);				 // We will manipulate the readers counter
-//		readers--;							 // We are leaving, there is one less reader
-//		if (readers == 0)				// If there are no more readers currently reading...
-//			sem_post(&accessM);				// ...release exclusive access to the resource
-//		sem_post(&readresM);				 // We are done accessing the number of readers for now
-//	}
+
 }
 
 void writer(char *name)
-{
+{	
 	while(true)
-	{
-		WaitForSingleObject(semWrite,INFINITE);
+	{	flag=1;
+		WaitForSingleObject(a,INFINITE);
+		flag=0;
 		Sleep(rand()%500);
-		container+=5;
-		printf("%s wrote,value:%d\n\n",name,container);
-		Sleep(rand()%2500);
-		ReleaseSemaphore(semWrite,1,NULL);
-		
-		
-		
+		mtx.lock();
+		cout<<name<<" is working."<<endl;
+
+		mtx.unlock();
+
+		ReleaseSemaphore(a,1,NULL);
+		Sleep(rand()%500);
 		
 	}
 	
 	
 	
 	
-//	int num2=*(int*)prm;
-//	int j=0,r;
-//	for(j;j<iter;j++)
-//	{
-//		if(sem_wait(&orderM)==0) printf("%d Писатель %d в очереди__________П%d\n",j,num2,num2); // Remember our order of arrival
-//		sem_wait(&accessM);					// Request exclusive access to the resource
-//		sem_post(&orderM);					 // Release order of arrival semaphore (we have been served)
-//
-//		printf("%d Работает писатель %d________________П%d\n",j,num2,num2);				 // Here the writer can modify the resource at will
-//		r=1+rand()%4;
-//		sleep(r);
-//		sem_post(&accessM);					// Release exclusive access to the resource
-//	}
+
 }
 
 
